995d3e6cd301f36dd421e8c8ff141b087ce0c0b1
Jeremy Bertin <jeremybertin2@gmail.com>
2017-04-12 12:44:34 -0400

    Started basic menu and added basic menu graphics and fonts. Launcher scene is the main menu, loby scene is where the players join to start a match and results scene is the end game scene that shows scores

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void StartGame()
+    {
+
+    }
+
+    public void JoinGame()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+}
