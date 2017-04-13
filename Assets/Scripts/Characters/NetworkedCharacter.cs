7bcc11bcea77e9d8513346c1c60ba87750a47fe1
Jeremy Bertin <jeremybertin2@gmail.com>
2017-04-12 23:28:36 -0400

    Implemented most of the networking, curently only works in it's own test scene. Added a mob AI that chases the player for a certain amount of time and just patrols a predefined path or else. Implemented connecting to a game, Loading levels on all clients, item pickup, bullet instantiation, NPC Syncronization, NPC will chase all players not just local.

---
+++ Assets/Scripts/Characters/NetworkedCharacter.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedCharacter : Photon.PunBehaviour
+{
+    [SerializeField]
+    protected CharacterStats playerStats;
+
+    //Components
+    public Collider ThisCollider { get; private set; }
+    public Rigidbody ThisRigidBody { get; private set; }
+
+    // Use this for initialization
+    public virtual void Start()
+    {
+        ThisCollider = GetComponent<Collider>();
+        ThisRigidBody = GetComponent<Rigidbody>();
+    }
+
+    public bool IsDead()
+    {
+        if (playerStats.Health <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    protected void Die()
+    {
+    }
+
+
+    public virtual CharacterStats PlayerStats
+    {
+        get
+        {
+            return playerStats;
+        }
+
+        set
+        {
+            playerStats = value;
+        }
+    }
+
+    /*Functions To Alter Stats */
+    public virtual void removeHealth(int value)
+    {
+        playerStats.Health -= value;
+
+    }
+
+    public virtual void addHealth(int value)
+    {
+        playerStats.Health += value;
+
+    }
+
+    public virtual void removeMaxHealth(int value)
+    {
+        playerStats.MaxHealth -= value;
+
+    }
+
+    public virtual void addMaxHealth(int value)
+    {
+        playerStats.MaxHealth += value;
+
+    }
+
+    public void removeMovementSpeed(int value)
+    {
+        playerStats.MovementSpeed -= value;
+    }
+
+    public void addMovementSpeed(int value)
+    {
+        playerStats.MovementSpeed += value;
+    }
+
+    /* Damage */
+
+    //Deals with removing health, activating damage animations, and checking if dead then trigger death
+    private void TakeDamage(Damage damage)
+    {
+
+        if (damage.DamageValue > 0)
+        {
+            removeHealth(damage.DamageValue);
+
+            //Create a UI Damage Pop up
+            DamagePopUpController.CreateDamagePopUp(damage.DamageValue.ToString(), new Vector3(transform.position.x, transform.position.y + ThisCollider.bounds.size.y, transform.position.z), damage.damageType);
+
+            if (IsDead())
+            {
+                Die();
+            }
+        }
+    }
+
+    //Calculates damage based on An Attack
+    private Damage CalculateDamage(Attack attack)
+    {
+        Damage damage = new Damage();
+
+        //Calculate Damage This can later be used to block damage if Player has a shield or something like that etc.
+        damage.DamageValue = Mathf.RoundToInt(attack.BaseDamage);
+
+        return damage;
+    }
+
+    //Coroutine to take damage from an attack over time damage dealer
+    private IEnumerator doDamageOverTime(Attack attack)
+    {
+
+        while (attack.DamageOvertime.totalDuration > Time.time)
+        {
+            yield return new WaitForSeconds(attack.DamageOvertime.Intervals);
+            TakeDamage(new Damage(attack.DamageOvertime.Damage, attack.DamageType));
+        }
+
+        attack.DamageOvertime.DamageInstances--;
+
+    }
+
+
+    //For Trigger Collisions
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if (!IsDead())
+        {
+
+            //If character can take damage from these sources
+            if (other.gameObject.layer == LayerMask.NameToLayer("Projectile") || other.gameObject.layer == LayerMask.NameToLayer("Trap"))
+            {
+                if (other.gameObject.GetComponent<DamageDealer>() != null)
+                {
+                    //Get Attack Component From Other object
+                    Attack attack = other.gameObject.GetComponent<DamageDealer>().Attack;
+
+                    TakeDamage(CalculateDamage(attack));
+
+
+                    //Check if this attack apllies damage overtime Also 
+                    if ((attack.DamageOvertime.IsDamageOvertime))
+                    {
+                        if (attack.DamageOvertime.DamageInstances > 0)
+                        {
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration;
+                        }
+                        else
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration + Time.time;
+
+
+                        attack.DamageOvertime.DamageInstances++;
+                        //Start coroutine that will damage this character at each interval until duration is over
+                        StartCoroutine(doDamageOverTime(attack));
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    //For typical Collisions
+    public virtual void OnCollisionEnter(Collision other)
+    {
+        if (!IsDead())
+        {
+            //If character can take damage from this source
+            if (other.gameObject.layer == LayerMask.NameToLayer("Projectile") || other.gameObject.layer == LayerMask.NameToLayer("Trap"))
+            {
+                if (other.gameObject.GetComponent<DamageDealer>() != null)
+                {
+                    //Get Attack Component From Other object
+                    Attack attack = other.gameObject.GetComponent<DamageDealer>().Attack;
+
+                    TakeDamage(CalculateDamage(attack));
+
+
+                    //Check if this attack apllies damage overtime Also 
+                    if ((attack.DamageOvertime.IsDamageOvertime))
+                    {
+                        if (attack.DamageOvertime.DamageInstances > 0)
+                        {
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration;
+                        }
+                        else
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration + Time.time;
+
+
+                        attack.DamageOvertime.DamageInstances++;
+                        //Start coroutine that will damage this character at each interval until duration is over
+                        StartCoroutine(doDamageOverTime(attack));
+                    }
+                }
+            }
+        }
+    }
+
+
+    public virtual void OnParticleCollision(GameObject other)
+    {
+
+        if (!IsDead())
+        {
+            //If character can take damage from this source
+            if (other.gameObject.layer == LayerMask.NameToLayer("Projectile") || other.gameObject.layer == LayerMask.NameToLayer("Trap"))
+            {
+                if (other.gameObject.GetComponent<DamageDealer>() != null)
+                {
+                    //Get Attack Component From Other object
+                    Attack attack = other.gameObject.GetComponent<DamageDealer>().Attack;
+
+                    TakeDamage(CalculateDamage(attack));
+
+
+                    //Check if this attack apllies damage overtime Also 
+                    if ((attack.DamageOvertime.IsDamageOvertime))
+                    {
+                        if (attack.DamageOvertime.DamageInstances > 0)
+                        {
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration;
+                        }
+                        else
+                            attack.DamageOvertime.totalDuration = attack.DamageOvertime.Duration + Time.time;
+
+
+                        attack.DamageOvertime.DamageInstances++;
+                        //Start coroutine that will damage this character at each interval until duration is over
+                        StartCoroutine(doDamageOverTime(attack));
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Networking/PlayerNetTest.cs
@@ -0,0 +1,472 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerNetTest : NetworkedCharacter, IPunObservable
+{
+
+    //For Networking
+    public static GameObject localPlayerInstance;
+
+    //UI
+    public GameObject PlayerUIPrefab;
+
+    //UI
+    public GameObject BulletPrefab;
+
+    [SerializeField]
+    private string playerName;
+
+    [SerializeField]
+    private int playerNumber;
+
+    [SerializeField]
+    private int playerModelNumber;
+
+    [SerializeField]
+    private Ability currentAbility;
+
+    [SerializeField]
+    private Weapon currentWeapon;
+
+    [SerializeField]
+    private PlayerColor playerColor;
+
+    [SerializeField]
+    private PlayerControllerNumber playerControllerNumber = PlayerControllerNumber.Player1;
+
+    [SerializeField]
+    private bool localPlayer;
+
+    private int score;
+
+    private Game parentGame;
+
+    private PlayerController playerController;
+
+    //Initialisers In case they are needed to set up values
+    public void Initialize()
+    {
+        playerNumber = 0;
+        playerModelNumber = 0;
+        playerStats = new CharacterStats(0, 0, 0);
+        currentAbility = new Ability();
+        currentWeapon = new Weapon();
+        playerColor = PlayerColor.Red;
+    }
+
+
+    public void Initialize(int playerNumber, int playerModelNumber,CharacterStats stats, Ability ability, Weapon weapon, PlayerColor color, Game parentGame)
+    {
+        this.parentGame = parentGame;
+        this.playerNumber = playerNumber;
+        this.playerModelNumber = playerModelNumber;
+        playerStats = stats;
+        currentAbility = ability;
+        currentWeapon = weapon;
+        playerColor = color;
+    }
+
+    void Awake()
+    {
+        //Static instance of the Local Player GameObject
+        if (photonView.isMine)
+            PlayerNetworked.localPlayerInstance = this.gameObject;
+
+        //Don't destroy When you load a new scene
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    public override void Start () {
+
+        base.Start();
+
+        if (localPlayer)
+        {
+            //by default set to local player
+            setUpPlayerController(new Networked(gameObject));
+        }
+
+        //UI
+        if (PlayerUIPrefab != null)
+        {
+            GameObject ui = Instantiate(PlayerUIPrefab) as GameObject;
+
+            //Call the SetTarget Method of this prefab and send it this player
+            ui.SendMessage("SetTarget", gameObject, SendMessageOptions.RequireReceiver);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //If my client controls this player
+        if (photonView.isMine)
+        {
+            if (playerController != null)
+                playerController.moveInput();
+        }
+    }
+
+    //Network message call
+    [PunRPC]
+    void DestroyPhotonView(int photonViewId)
+    {
+        if (PhotonNetwork.isMasterClient)
+            PhotonNetwork.Destroy(PhotonView.Find(photonViewId));
+    }
+
+    //Networking
+    //Send updates of change of certain data
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //Send My client
+            stream.SendNext(PlayerStats.Health);
+        }
+        else
+        {
+            //Receive other users data
+            PlayerStats.Health = (int)stream.ReceiveNext();
+        }
+    }
+
+    public void setUpPlayerController(PlayerController controller)
+    {
+        playerController = controller;
+    }
+
+    /*Functions To Alter Stats */
+    public override void removeHealth(int value)
+    {
+        if (photonView.isMine){
+        base.removeHealth(value);
+
+        if (parentGame != null)
+            parentGame.PlayerUpdateHealth(playerNumber, playerStats.Health);
+        }
+    }
+
+    public override void addHealth(int value)
+    {
+        if (photonView.isMine)
+        {
+            base.addHealth(value);
+
+            if (parentGame != null)
+                parentGame.PlayerUpdateHealth(playerNumber, playerStats.Health);
+        }
+    }
+
+    public override void removeMaxHealth(int value)
+    {
+        if (photonView.isMine)
+        {
+            base.removeMaxHealth(value);
+
+            if (parentGame != null)
+                parentGame.PlayerUpdateMaxHealth(playerNumber, playerStats.MaxHealth);
+        }
+    }
+
+    public override void addMaxHealth(int value)
+    {
+        if (photonView.isMine)
+        {
+            base.addMaxHealth(value);
+
+            if (parentGame != null)
+                parentGame.PlayerUpdateMaxHealth(playerNumber, playerStats.MaxHealth);
+        }
+    }
+
+
+    /*Functions To Alter ammo count */
+    public void removeWeaponAmmo(int num)
+    {
+        if (photonView.isMine)
+        {
+            if (currentWeapon.AmmoCount != Infinity.InfinityValue())
+            {
+                currentWeapon.AmmoCount -= num;
+
+                if (parentGame != null)
+                    parentGame.PlayerUpdateAmmoCount(playerNumber, currentWeapon.AmmoCount);
+            }
+        }
+       
+    }
+
+    public void addWeaponAmmo(int num)
+    {
+        if (photonView.isMine)
+        {
+            if (currentWeapon.AmmoCount != Infinity.InfinityValue())
+            {
+                currentWeapon.AmmoCount += num;
+
+                if (parentGame != null)
+                    parentGame.PlayerUpdateAmmoCount(playerNumber, currentWeapon.AmmoCount);
+            }
+        }
+    }
+
+    public void removeScore(int value)
+    {
+        Score -= value;
+    }
+
+    public  void addScore(int value)
+    {
+        Score += value;
+    }
+
+    public bool weaponHasAmmo()
+    {
+        if (CurrentWeapon.AmmoCount > 0)
+            return true;
+        else
+            return false;
+    }
+
+
+    /* Damage */
+
+    /* Damage Trigger */
+
+    //For Trigger Collisions
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (photonView.isMine)
+        {
+            base.OnTriggerEnter(other);
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("Specials"))
+            {
+                AbilityObject ability;
+                WeaponObject weapon;
+
+                if (ability = other.gameObject.GetComponent<AbilityObject>())
+                {
+                    CurrentAbility = ability.Ability;
+                }
+                else if (weapon = other.gameObject.GetComponent<WeaponObject>())
+                {
+                    CurrentWeapon = weapon.Weapon;
+                }
+
+                    photonView.RPC("DestroyPhotonView", PhotonTargets.All, other.gameObject.GetComponent<PhotonView>().viewID);
+            }
+
+        }
+    }
+
+    //For typical Collisions
+    public override void OnCollisionEnter(Collision other)
+    {
+        if (photonView.isMine)
+        {
+            base.OnCollisionEnter(other);
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("Specials"))
+            {
+                AbilityObject ability;
+                WeaponObject weapon;
+
+                if (ability = other.gameObject.GetComponent<AbilityObject>())
+                {
+                    CurrentAbility = ability.Ability;
+                }
+                else if (weapon = other.gameObject.GetComponent<WeaponObject>())
+                {
+                    CurrentWeapon = weapon.Weapon;
+                }
+            }
+        }
+    }
+
+    public override void OnParticleCollision(GameObject other)
+    {
+        if (photonView.isMine)
+        {
+            base.OnParticleCollision(other);
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("Specials"))
+            {
+                AbilityObject ability;
+                WeaponObject weapon;
+
+                if (ability = other.gameObject.GetComponent<AbilityObject>())
+                {
+                    CurrentAbility = ability.Ability;
+                }
+                else if (weapon = other.gameObject.GetComponent<WeaponObject>())
+                {
+                    CurrentWeapon = weapon.Weapon;
+                }
+            }
+        }
+    }
+
+    public int PlayerNumber
+    {
+        get
+        {
+            return playerNumber;
+        }
+
+        set
+        {
+            playerNumber = value;
+            UpdatePlayer();
+        }
+    }
+
+    public override CharacterStats PlayerStats
+    {
+        get
+        {
+            return base.PlayerStats;
+        }
+
+        set
+        {
+            base.PlayerStats = value;
+            UpdatePlayer();
+        }
+    }
+
+    public Ability CurrentAbility
+    {
+        get
+        {
+            return currentAbility;
+        }
+
+        set
+        {
+            currentAbility = value;
+
+            if (parentGame != null)
+                parentGame.PlayerUpdateAbility(playerNumber, currentAbility);
+        }
+    }
+
+    public Weapon CurrentWeapon
+    {
+        get
+        {
+            return currentWeapon;
+        }
+
+        set
+        {
+            currentWeapon = value;
+
+            if (parentGame != null)
+            {
+                parentGame.PlayerUpdateWeapon(playerNumber, currentWeapon);
+                parentGame.PlayerUpdateAmmoCount(playerNumber, currentWeapon.AmmoCount);
+            }
+                
+        }
+    }
+
+    public PlayerColor PlayerColor
+    {
+        get
+        {
+            return playerColor;
+        }
+
+        set
+        {
+            playerColor = value;
+
+            if (parentGame != null)
+                parentGame.PlayerUpdateColor(playerNumber, playerColor);
+        }
+    }
+
+    public Game ParentGame
+    {
+        get
+        {
+            return parentGame;
+        }
+
+        set
+        {
+            parentGame = value;
+        }
+    }
+
+    public int PlayerModelNumber
+    {
+        get
+        {
+            return playerModelNumber;
+        }
+
+        set
+        {
+            playerModelNumber = value;
+
+            if(parentGame != null)
+            parentGame.PlayerUpdateModel(playerNumber, playerModelNumber);
+        }
+    }
+
+    public string PlayerName
+    {
+        get
+        {
+            return playerName;
+        }
+
+        set
+        {
+            playerName = value;
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+        }
+    }
+
+    public PlayerControllerNumber PlayerControllerNumber
+    {
+        get
+        {
+            return playerControllerNumber;
+        }
+
+        set
+        {
+            playerControllerNumber = value;
+        }
+    }
+
+    public void UpdatePlayer()
+    {
+        if (photonView.isMine)
+        {
+        }
+    }
+
+    public void fire()
+    {
+        PhotonNetwork.Instantiate(BulletPrefab.name, transform.position, Quaternion.identity, 0);
+    }
+}
