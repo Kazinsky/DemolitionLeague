2853988e2c0a8fda5356efd35096a76744b33b18
Heidy Dallard <heidy.dallard@epitech.eu>
2017-04-13 02:37:06 -0400

    Menu almost finished
    All possible interactions are prepared, just need to start the scenes in
    local and join the game in multiplayer

---
+++ Assets/Scripts/UI/MenuView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuView : MonoBehaviour {
+
+	public GameObject previousView;
+
+	public void previous() {
+		gameObject.SetActive (false);
+		previousView.SetActive (true);
+	}
+}
