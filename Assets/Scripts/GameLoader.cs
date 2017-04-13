3efbe7ce51781bc8273acc77cca1979dea0f750d
Heidy Dallard <heidy.dallard@epitech.eu>
2017-04-13 02:37:06 -0400

    Menu almost finished
    All possible interactions are prepared, just need to start the scenes in
    local and join the game in multiplayer

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour {
+	int levelToLoad;
+	int maxPlayers = 2;
+	int nbPlayers = 1;
+	int nbAi = 1;
+
+	public void setLevelToLoad(int level) {
+		levelToLoad = level;
+	}
+
+	public void setMaxPlayers(int max) {
+		maxPlayers = max;
+	}
+
+	public void setNbPlayers(int players) {
+		nbPlayers = players;
+		nbAi = maxPlayers - nbPlayers;
+	}
+
+	public void start() {
+	}
+}
